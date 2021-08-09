74efaccc5fc6bd99d60df4d3ca3c6d4491298fce
Gary Chu <ma.gary729@gmail.com>
2021-08-09 11:12:07 +0800

    fix: const -> let

--- src/get-previous-meeting-time.js
+++ src/get-previous-meeting-time.js
@@ -2,7 +2,7 @@ import dayjs from 'dayjs'
 
 export const getPreviousMeetingTime = () => {
   const { MEETING_DAY = '1,2,3,4,5', MEETING_TIME = '10:00', MEETING_TIME_ZONE = '+08:00' } = process.env
-  const queryDay = dayjs()
+  let queryDay = dayjs()
   
   const getMeetingDateTime = (date) => `${date.format('YYYY-MM-DD')}T${MEETING_TIME}:00${MEETING_TIME_ZONE}`
   
